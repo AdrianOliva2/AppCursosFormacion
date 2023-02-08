df6b99a9a660e2e1af809ea47927d363dc6496d2
AdrianOliva2 <aolirio@g.educaand.es>
2023-02-08 20:28:57 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppCursosFormacion',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
